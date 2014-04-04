6ffb8b77857c430b728ce8e2f52244e77556ecd6
dhcarmona <dhcarmona@gmail.com>
2014-04-03 20:46:52 -0600

    Subida inicial proyecto Rails

---
+++ SEMCR Backend/SEMCR/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SEMCR::Application.config.secret_key_base = '20f04a87a67ca13f90fdc73bdb11e1797d4c0d9a93a94501528179a2a44726de7ac9ad69192e14843926c26138f0f55dd9a2565aa8027d9b3d6912c1f9d5dc1b'
